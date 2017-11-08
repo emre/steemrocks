1be1db33b659f1725ffc1560705addd240054a45
Emre Yilmaz <emre.yilmaz@metglobal.com>
2017-11-08 16:18:21 +0300

    Fix pagination again

--- steemrocks/utils.py
+++ steemrocks/utils.py
@@ -48,12 +48,12 @@ class Pagination(object):
 
     @property
     def has_next(self):
-        return self.page  < self.pages
+        return self.page < self.pages
 
     def iter_pages(self, left_edge=2, left_current=2,
                    right_current=5, right_edge=2):
         last = 0
-        for num in range(1, self.pages):
+        for num in range(1, self.pages + 1):
             if num <= left_edge or \
                (num > self.page - left_current - 1 and \
                 num < self.page + right_current) or \
